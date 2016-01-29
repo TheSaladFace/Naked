83e2f7df9245f9318a9736ffb0c13a77ba380201
TheSaladFace <roates@gmail.com>
2016-01-29 17:08:21 +0000

    Cleaned Section Shortcode
    Cleaned up the section shortcode

--- framework-customizations/extensions/shortcodes/shortcodes/section/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/section/views/view.php
@@ -1,34 +1,37 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
+<?php
+/**
+ * Modifies the section shortcode
+ */
 
+/** Dont run without Unyson plugin **/
+if (!defined('FW')) die('Forbidden');
+
+/** Paths **/
 $uri = fw_get_template_customizations_directory_uri('/extensions/shortcodes/shortcodes/section');
 
+/** Background settings from options **/
 $bg_color = '';
 if ( ! empty( $atts['background_color'] ) ) {
 	$bg_color = 'background-color:' . $atts['background_color'] . ';';
 }
-
 $bg_image = '';
 if ( ! empty( $atts['background_image'] ) && ! empty( $atts['background_image']['data']['icon'] ) ) {
 	$bg_image = 'background-image:url(' . $atts['background_image']['data']['icon'] . ');';
 }
-
 $bg_position = '';
 if ( ! empty( $atts['background_position'] )) {
 	$bg_position = 'background-position:' . $atts['background_position'] .';';
 }
-
 $bg_repeat = '';
 if ( ! empty( $atts['background_repeat'] )) {
 	$bg_repeat = 'background-repeat:' . $atts['background_repeat'] .';';
 }
-
 $bg_size = '';
 if ( ! empty( $atts['background_size'] )) {
 	$bg_size = 'background-size:' . $atts['background_size'] .';';
 }
 
+/** Margins and padding from options **/
 $padding_top ='';
 if ( ! empty( $atts['padding_top'] ))
 {
@@ -37,7 +40,6 @@ if ( ! empty( $atts['padding_top'] ))
 		$padding_top = ' padding-top';
 	}
 }
-
 $padding_bottom ='';
 if ( ! empty( $atts['padding_bottom'] ))
 {
@@ -46,7 +48,6 @@ if ( ! empty( $atts['padding_bottom'] ))
 		$padding_bottom = ' padding-bottom';
 	}
 }
-
 $margin_top ='';
 if ( ! empty( $atts['margin_top'] ))
 {
@@ -55,7 +56,6 @@ if ( ! empty( $atts['margin_top'] ))
 		$margin_top = ' margin-top';
 	}
 }
-
 $margin_bottom ='';
 if ( ! empty( $atts['margin_bottom'] ))
 {
@@ -65,15 +65,17 @@ if ( ! empty( $atts['margin_bottom'] ))
 	}
 }
 
+/** Parallax background from options **/
 $bg_attachment='';
 $section_parallax_string='';
 if ( ! empty( $atts['background_parallax_ratio'] ) ) {
-	wp_enqueue_script('naked-stellar', $uri . '/static/js/jquery.stellar.min.js',array('jquery'),'',true ); //will enqueue from the default theme location but will load from here if theme is changed
-	wp_enqueue_script('naked-stellar-init', $uri . '/static/js/stellar-init.js',array('jquery'),'',true );
-	$section_parallax_string='data-stellar-background-ratio="'.$atts['background_parallax_ratio'].'"';	
+	wp_enqueue_script('thshpr-stellar', $uri . '/static/js/jquery.stellar.min.js',array('jquery'),'',true ); //will enqueue from the default theme location but will load from here if theme is changed
+	wp_enqueue_script('thshpr-stellar-init', $uri . '/static/js/stellar-init.js',array('jquery'),'',true );
+	$section_parallax_string='data-stellar-background-ratio="'.$atts['background_parallax_ratio'].'"';
 	$bg_attachment= 'background-attachment: fixed;'; //set this to fixed for stellar
 }
 
+/** Video background from options (unchanged from default unyson) **/
 $bg_video_data_attr    = '';
 $section_extra_classes = '';
 if ( ! empty( $atts['video'] ) ) {
@@ -84,14 +86,15 @@ if ( ! empty( $atts['video'] ) ) {
 	$section_extra_classes .= ' background-video';
 }
 
+/** Only parallax if set in options **/
 if ( ! empty( $atts['background_parallax_ratio'] ) ) {
 	$section_extra_classes .= ' parallax-bg';
 }
 
+/** Generate Strings **/
 $section_style   = ( $bg_color || $bg_image || $bg_position || $bg_size ) ? 'style="' . esc_attr($bg_color . $bg_image . $bg_position . $bg_repeat . $bg_size . $bg_attachment) . '"' : '';
 $container_class = ( isset( $atts['is_fullwidth'] ) && $atts['is_fullwidth'] ) ? 'fw-container-fluid' : 'fw-container';
 $container_class.=$padding_top.$padding_bottom.$margin_top.$margin_bottom;
-
 ?>
 <section class="fw-main-row <?php echo esc_attr($section_extra_classes) ?>" <?php echo $section_style; ?> <?php echo $bg_video_data_attr; ?> <?php echo $section_parallax_string; ?>>
 	<div class="<?php echo esc_attr($container_class); ?>">
