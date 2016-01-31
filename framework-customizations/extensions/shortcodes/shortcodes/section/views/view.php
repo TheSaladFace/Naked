c7efc06ed2bc41b87fb92a22dd359868cfa69aa1
TheSaladFace <roates@gmail.com>
2016-01-31 17:40:49 +0000

    Clean up for Section Shortcode

--- framework-customizations/extensions/shortcodes/shortcodes/section/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/section/views/view.php
@@ -6,32 +6,33 @@
 /** Dont run without Unyson plugin **/
 if (!defined('FW')) die('Forbidden');
 
-/** Paths **/
 $uri = fw_get_template_customizations_directory_uri('/extensions/shortcodes/shortcodes/section');
 
-/** Background settings from options **/
 $bg_color = '';
 if ( ! empty( $atts['background_color'] ) ) {
 	$bg_color = 'background-color:' . $atts['background_color'] . ';';
 }
+
 $bg_image = '';
 if ( ! empty( $atts['background_image'] ) && ! empty( $atts['background_image']['data']['icon'] ) ) {
 	$bg_image = 'background-image:url(' . $atts['background_image']['data']['icon'] . ');';
 }
+
 $bg_position = '';
 if ( ! empty( $atts['background_position'] )) {
 	$bg_position = 'background-position:' . $atts['background_position'] .';';
 }
+
 $bg_repeat = '';
 if ( ! empty( $atts['background_repeat'] )) {
 	$bg_repeat = 'background-repeat:' . $atts['background_repeat'] .';';
 }
+
 $bg_size = '';
 if ( ! empty( $atts['background_size'] )) {
 	$bg_size = 'background-size:' . $atts['background_size'] .';';
 }
 
-/** Margins and padding from options **/
 $padding_top ='';
 if ( ! empty( $atts['padding_top'] ))
 {
@@ -40,6 +41,7 @@ if ( ! empty( $atts['padding_top'] ))
 		$padding_top = ' padding-top';
 	}
 }
+
 $padding_bottom ='';
 if ( ! empty( $atts['padding_bottom'] ))
 {
@@ -48,6 +50,7 @@ if ( ! empty( $atts['padding_bottom'] ))
 		$padding_bottom = ' padding-bottom';
 	}
 }
+
 $margin_top ='';
 if ( ! empty( $atts['margin_top'] ))
 {
@@ -56,6 +59,7 @@ if ( ! empty( $atts['margin_top'] ))
 		$margin_top = ' margin-top';
 	}
 }
+
 $margin_bottom ='';
 if ( ! empty( $atts['margin_bottom'] ))
 {
@@ -65,17 +69,15 @@ if ( ! empty( $atts['margin_bottom'] ))
 	}
 }
 
-/** Parallax background from options **/
 $bg_attachment='';
 $section_parallax_string='';
 if ( ! empty( $atts['background_parallax_ratio'] ) ) {
 	wp_enqueue_script('thshpr-stellar', $uri . '/static/js/jquery.stellar.min.js',array('jquery'),'',true ); //will enqueue from the default theme location but will load from here if theme is changed
-	wp_enqueue_script('thshpr-stellar-init', $uri . '/static/js/stellar-init.js',array('jquery'),'',true );
+	wp_enqueue_script('thshpr-stellar-init', $uri . '/static/js/stellar-init.js',array('jquery','thshpr-stellar'),'',true );
 	$section_parallax_string='data-stellar-background-ratio="'.$atts['background_parallax_ratio'].'"';
 	$bg_attachment= 'background-attachment: fixed;'; //set this to fixed for stellar
 }
 
-/** Video background from options (unchanged from default unyson) **/
 $bg_video_data_attr    = '';
 $section_extra_classes = '';
 if ( ! empty( $atts['video'] ) ) {
@@ -86,15 +88,14 @@ if ( ! empty( $atts['video'] ) ) {
 	$section_extra_classes .= ' background-video';
 }
 
-/** Only parallax if set in options **/
 if ( ! empty( $atts['background_parallax_ratio'] ) ) {
 	$section_extra_classes .= ' parallax-bg';
 }
 
-/** Generate Strings **/
 $section_style   = ( $bg_color || $bg_image || $bg_position || $bg_size ) ? 'style="' . esc_attr($bg_color . $bg_image . $bg_position . $bg_repeat . $bg_size . $bg_attachment) . '"' : '';
 $container_class = ( isset( $atts['is_fullwidth'] ) && $atts['is_fullwidth'] ) ? 'fw-container-fluid' : 'fw-container';
 $container_class.=$padding_top.$padding_bottom.$margin_top.$margin_bottom;
+
 ?>
 <section class="fw-main-row <?php echo esc_attr($section_extra_classes) ?>" <?php echo $section_style; ?> <?php echo $bg_video_data_attr; ?> <?php echo $section_parallax_string; ?>>
 	<div class="<?php echo esc_attr($container_class); ?>">
