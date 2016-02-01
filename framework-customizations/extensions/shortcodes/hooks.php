5304bd245ec07676624fc23cdd3f11be266171d7
TheSaladFace <roates@gmail.com>
2016-02-01 16:06:47 +0000

    Shortcode Hooks File Cleaned
    Shortcode hooks file had superfluous hooks and masonry image creation
    hook moved to hooks file in its sub directory

--- framework-customizations/extensions/shortcodes/hooks.php
+++ framework-customizations/extensions/shortcodes/hooks.php
@@ -7,33 +7,4 @@ function _filter_disable_shortcodes($to_disable)
 //	$to_disable[] = 'some_other_shortcode';
 	return $to_disable;
 }
-add_filter('fw_ext_shortcodes_disable_shortcodes', '_filter_disable_shortcodes');
-
-
-add_action( 'init', 'naked_masonry_image_setup' );
-function naked_masonry_image_setup() {
-   add_image_size( 'masonry', 800, 500, false );
-}
-
-add_filter('next_posts_link_attributes', 'naked_posts_link_attributes_next');
-add_filter('previous_posts_link_attributes', 'naked_posts_link_attributes_prev');
-
-function naked_posts_link_attributes_next() {
-    return 'class="next-post"';
-}
-function naked_posts_link_attributes_prev() {
-    return 'class="prev-post"';
-}
-
-/**
- * Disables built in Unyson shortcodes
- */
-/*
-function _filter_theme_disable_default_shortcodes($to_disable) {
-    $to_disable[] = 'accordion';
-    $to_disable[] = 'button';
-
-    return $to_disable;
-}
-add_filter('fw_ext_shortcodes_disable_shortcodes', '_filter_theme_disable_default_shortcodes');
-*/
+add_filter('thshpr_ext_shortcodes_disable_shortcodes', '_filter_disable_shortcodes');
