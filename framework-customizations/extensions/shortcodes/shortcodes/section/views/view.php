4b06c609e97137c001c61c05026cbdd65029955a
TheSaladFace <roates@gmail.com>
2016-02-06 17:58:33 +0000

    Adjusted for renamed option variables

--- framework-customizations/extensions/shortcodes/shortcodes/section/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/section/views/view.php
@@ -9,61 +9,61 @@ if (!defined('FW')) die('Forbidden');
 $uri = fw_get_template_customizations_directory_uri('/extensions/shortcodes/shortcodes/section');
 
 $bg_color = '';
-if ( ! empty( $atts['background_color'] ) ) {
-	$bg_color = 'background-color:' . $atts['background_color'] . ';';
+if ( ! empty( $atts['opt_background_color'] ) ) {
+	$bg_color = 'background-color:' . $atts['opt_background_color'] . ';';
 }
 
 $bg_image = '';
-if ( ! empty( $atts['background_image'] ) && ! empty( $atts['background_image']['data']['icon'] ) ) {
-	$bg_image = 'background-image:url(' . $atts['background_image']['data']['icon'] . ');';
+if ( ! empty( $atts['opt_background_image'] ) && ! empty( $atts['opt_background_image']['data']['icon'] ) ) {
+	$bg_image = 'background-image:url(' . $atts['opt_background_image']['data']['icon'] . ');';
 }
 
 $bg_position = '';
-if ( ! empty( $atts['background_position'] )) {
-	$bg_position = 'background-position:' . $atts['background_position'] .';';
+if ( ! empty( $atts['opt_background_position'] )) {
+	$bg_position = 'background-position:' . $atts['opt_background_position'] .';';
 }
 
 $bg_repeat = '';
-if ( ! empty( $atts['background_repeat'] )) {
-	$bg_repeat = 'background-repeat:' . $atts['background_repeat'] .';';
+if ( ! empty( $atts['opt_background_repeat'] )) {
+	$bg_repeat = 'background-repeat:' . $atts['opt_background_repeat'] .';';
 }
 
 $bg_size = '';
-if ( ! empty( $atts['background_size'] )) {
-	$bg_size = 'background-size:' . $atts['background_size'] .';';
+if ( ! empty( $atts['opt_background_size'] )) {
+	$bg_size = 'background-size:' . $atts['opt_background_size'] .';';
 }
 
 $padding_top ='';
-if ( ! empty( $atts['padding_top'] ))
+if ( ! empty( $atts['opt_padding_top'] ))
 {
-	if($atts['padding_top']=="Yes")
+	if($atts['opt_padding_top']=="Yes")
 	{
 		$padding_top = ' padding-top';
 	}
 }
 
 $padding_bottom ='';
-if ( ! empty( $atts['padding_bottom'] ))
+if ( ! empty( $atts['opt_padding_bottom'] ))
 {
-	if($atts['padding_bottom']=="Yes")
+	if($atts['opt_padding_bottom']=="Yes")
 	{
 		$padding_bottom = ' padding-bottom';
 	}
 }
 
 $margin_top ='';
-if ( ! empty( $atts['margin_top'] ))
+if ( ! empty( $atts['opt_margin_top'] ))
 {
-	if($atts['margin_top']=="Yes")
+	if($atts['opt_margin_top']=="Yes")
 	{
 		$margin_top = ' margin-top';
 	}
 }
 
 $margin_bottom ='';
-if ( ! empty( $atts['margin_bottom'] ))
+if ( ! empty( $atts['opt_margin_bottom'] ))
 {
-	if($atts['margin_bottom']=="Yes")
+	if($atts['opt_margin_bottom']=="Yes")
 	{
 		$margin_bottom = ' margin-bottom';
 	}
@@ -71,29 +71,29 @@ if ( ! empty( $atts['margin_bottom'] ))
 
 $bg_attachment='';
 $section_parallax_string='';
-if ( ! empty( $atts['background_parallax_ratio'] ) ) {
+if ( ! empty( $atts['opt_background_parallax_ratio'] ) ) {
 	wp_enqueue_script('thshpr-stellar'); //will enqueue from the default theme location but will load from here if theme is changed
 	wp_enqueue_script('thshpr-stellar-init');
-	$section_parallax_string='data-stellar-background-ratio="'.$atts['background_parallax_ratio'].'"';
+	$section_parallax_string='data-stellar-background-ratio="'.$atts['opt_background_parallax_ratio'].'"';
 	$bg_attachment= 'background-attachment: fixed;'; //set this to fixed for stellar
 }
 
 $bg_video_data_attr    = '';
 $section_extra_classes = '';
-if ( ! empty( $atts['video'] ) ) {
-	$filetype           = wp_check_filetype( $atts['video'] );
+if ( ! empty( $atts['opt_video'] ) ) {
+	$filetype           = wp_check_filetype( $atts['opt_video'] );
 	$filetypes          = array( 'mp4' => 'mp4', 'ogv' => 'ogg', 'webm' => 'webm', 'jpg' => 'poster' );
 	$filetype           = array_key_exists( (string) $filetype['ext'], $filetypes ) ? $filetypes[ $filetype['ext'] ] : 'video';
-	$bg_video_data_attr = 'data-wallpaper-options="' . fw_htmlspecialchars( json_encode( array( 'source' => array( $filetype => $atts['video'] ) ) ) ) . '"';
+	$bg_video_data_attr = 'data-wallpaper-options="' . fw_htmlspecialchars( json_encode( array( 'source' => array( $filetype => $atts['opt_video'] ) ) ) ) . '"';
 	$section_extra_classes .= ' background-video';
 }
 
-if ( ! empty( $atts['background_parallax_ratio'] ) ) {
+if ( ! empty( $atts['opt_background_parallax_ratio'] ) ) {
 	$section_extra_classes .= ' parallax-bg';
 }
 
 $section_style   = ( $bg_color || $bg_image || $bg_position || $bg_size ) ? 'style="' . esc_attr($bg_color . $bg_image . $bg_position . $bg_repeat . $bg_size . $bg_attachment) . '"' : '';
-$container_class = ( isset( $atts['is_fullwidth'] ) && $atts['is_fullwidth'] ) ? 'fw-container-fluid' : 'fw-container';
+$container_class = ( isset( $atts['opt_is_fullwidth'] ) && $atts['opt_is_fullwidth'] ) ? 'fw-container-fluid' : 'fw-container';
 $container_class.=$padding_top.$padding_bottom.$margin_top.$margin_bottom;
 
 ?>
