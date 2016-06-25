1ff8791f010b483675fd8dfa631ab78df848af71
TheSaladFace <roates@gmail.com>
2016-06-25 17:22:02 +0100

    Change Main Menu Name

--- functions.php
+++ functions.php
@@ -550,7 +550,7 @@ function naked_setup() {
     * This theme uses wp_nav_menu() in one location.
     */
     register_nav_menus( array(
-        'primary'  => __( 'Header bottom menu', 'naked' ),
+        'primary'  => __( 'Header Menu', 'naked' ),
     ) );
 
 
