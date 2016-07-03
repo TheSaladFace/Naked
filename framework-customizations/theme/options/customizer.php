c69a2e74e26d6ae10dd5c12c647f4a1ccfe0cb84
TheSaladFace <roates@gmail.com>
2016-07-03 17:19:49 +0100

    Added Company Social Icons

--- framework-customizations/theme/options/customizer.php
+++ framework-customizations/theme/options/customizer.php
@@ -6,15 +6,10 @@ $options = array(
 	'panel_1' => array(
         'title' => __('Header', 'thshpr'),
         'options' => array(
-
-
 			fw()->theme->get_options('header-options'),
-
-
-
         ),
     ),
-    'panel_2' => array(
+	'panel_2' => array(
         'title' => __('Posts', 'thshpr'),
         'options' => array(
 
@@ -70,12 +65,103 @@ $options = array(
 	'panel_3' => array(
         'title' => __('Typography', 'thshpr'),
         'options' => array(
-
-
 			fw()->theme->get_options('typography-options'),
-
-
-
+        ),
+    ),
+	'panel_4' => array(
+        'title' => __('Social', 'thshpr'),
+        'options' => array(
+			'opt_social_twitter' => array(
+				'label' => __( 'Your Twitter URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Twitter account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_facebook' => array(
+				'label' => __( 'Your Facebook URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Facebook account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_googleplus' => array(
+				'label' => __( 'Your Google Plus URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Google Plus account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_linkedin' => array(
+				'label' => __( 'Your Linked In URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Linked In account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_youtube' => array(
+				'label' => __( 'Your Youtube URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Youtube account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_pinterest' => array(
+				'label' => __( 'Your Pinterest URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Pinterest account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_instagram' => array(
+				'label' => __( 'Your Instagram URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Instagram account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_tumblr' => array(
+				'label' => __( 'Your Tumblr URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Tumblr account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_vine' => array(
+				'label' => __( 'Your Vine URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Vine account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_snapchat' => array(
+				'label' => __( 'Your Snapchat URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Snapchat account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_reddit' => array(
+				'label' => __( 'Your Reddit URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Reddit account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_flickr' => array(
+				'label' => __( 'Your Flickr URL', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter the URL of your Flickr account.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
+			'opt_social_email' => array(
+				'label' => __( 'Your Email Address', 'thshpr' ),
+				'type'  => 'text',
+				'value' => '',
+				'desc'  => __( 'Please enter your email address.','thshpr' ),
+				'help'  => __( 'Leave blank to remove', 'thshpr'  ),
+			),
         ),
     ),
 );
--- global-templates/header-nav.php
+++ global-templates/header-nav.php
@@ -54,16 +54,93 @@ else
 
 				</div>
 			</div><!--
-	--><?php if($show_search==1)
+	--><?php if($show_search==1||$show_social==1)
 	{
-		echo('<div class="fw-col-lg-1 vcenter-topbar">
-				<div class="search-container">
+		echo'<div class="fw-col-lg-1 vcenter-topbar">';
+
+			if($show_search==1&&$show_social==1)
+			{
+				$search_spacer_class="search-spacer";
+			}
+			else
+			{
+				$search_spacer_class="";
+			}
+			if($show_search==1)
+			{
+				echo'<div class="search-container">
 					<form class="searchbox">
 						<input type="search" placeholder="'.__('Type then press enter to search...', 'thshpr' ).'" name="search" class="searchbox-input" onkeyup="buttonUp();" required>
 						<span class="searchbox-icon"></span>
 					</form>
-				</div>
-			</div>');
+				</div>';
+			}
+
+			if($show_social==1)
+			{
+				echo'<div class="share-boxes featured-posts-grid-paragraph component-element header-social-icons '.$search_spacer_class.'">
+				    <div class="absolute-container">
+				        <i class="fa fa-at icon start-icon"></i></span><!--';
+						if(isset($social_facebook))
+						{
+				        	echo'--><span class="social-box facebook"><a class="inner facebook-inner" href="'.urlencode($social_facebook).'"><i class="fa fa-facebook icon"></i></a></span><!--';
+						}
+						if(isset($social_twitter))
+						{
+				        	echo'--><span class="social-box facebook"><a class="inner twitter-inner" href="'.urlencode($social_twitter).'"><i class="fa fa-twitter icon"></i></a></span><!--';
+						}
+						if(isset($social_googleplus))
+						{
+				        	echo'--><span class="social-box googleplus"><a class="inner google-plus-inner" href="'.urlencode($social_googleplus).'"><i class="fa fa-google-plus icon"></i></a></span><!--';
+						}
+						if(isset($social_linkedin))
+						{
+				        	echo'--><span class="social-box linkedin"><a class="inner linked-in-inner" href="'.urlencode($social_linkedin).'"><i class="fa fa-linkedin icon"></i></a></span><!--';
+						}
+						if(isset($social_youtube))
+						{
+				        	echo'--><span class="social-box youtube"><a class="inner youtube-inner" href="'.urlencode($social_youtube).'"><i class="fa fa-youtube icon"></i></a></span><!--';
+						}
+						if(isset($social_instagram))
+						{
+				        	echo'--><span class="social-box instagram"><a class="inner instagram-inner" href="'.urlencode($social_instagram).'"><i class="fa fa-instagram icon"></i></a></span><!--';
+						}
+						if(isset($social_pinterest))
+						{
+				        	echo'--><span class="social-box pinterest"><a class="inner pinterest-inner" href="'.urlencode($social_pinterest).'"><i class="fa fa-pinterest icon"></i></a></span><!--';
+						}
+						if(isset($social_tumblr))
+						{
+				        	echo'--><span class="social-box tumblr"><a class="inner tumblr-inner" href="'.urlencode($social_tumblr).'"><i class="fa fa-tumblr icon"></i></a></span><!--';
+						}
+						if(isset($social_vine))
+						{
+				        	echo'--><span class="social-box vine"><a class="inner vine-inner" href="'.urlencode($social_vine).'"><i class="fa fa-vine icon"></i></a></span><!--';
+						}
+						if(isset($social_snapchat))
+						{
+				        	echo'--><span class="social-box snapchat"><a class="inner snapchat-inner" href="'.urlencode($social_snapchat).'"><i class="fa fa-snapchat icon"></i></a></span><!--';
+						}
+						if(isset($social_reddit))
+						{
+				        	echo'--><span class="social-box reddit"><a class="inner reddit-inner" href="'.urlencode($social_reddit).'"><i class="fa fa-reddit icon"></i></a></span><!--';
+						}
+						if(isset($social_flickr))
+						{
+				        	echo'--><span class="social-box flickr"><a class="inner flickr-inner" href="'.urlencode($social_flickr).'"><i class="fa fa-flickr icon"></i></a></span><!--';
+						}
+						if(isset($social_email))
+						{
+				        	echo'--><span class="social-box email"><a class="inner email-inner" href="'.urlencode($social_email).'"><i class="fa fa-at icon"></i></a></span><!--';
+						}
+						echo'
+				    </div>
+				</div>';
+			}
+
+		echo'
+
+		</div>';
 	}
 	?>
 		</div>
