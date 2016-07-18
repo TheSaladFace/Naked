b5a9f88787cdcddd1e6e2221e15bffbe98cd40f2
TheSaladFace <roates@gmail.com>
2016-07-18 20:38:43 +0100

    Show / Hide header social and search

--- framework-customizations/theme/options/header-options.php
+++ framework-customizations/theme/options/header-options.php
@@ -10,7 +10,7 @@ $options = array(
 
 	'opt_header_show_search' =>array(
 		'type'  => 'switch',
-		'value' => 'Show',
+		'value' => '1',
 		'label' => __('Show Search Box in Header', 'thshpr'),
 		'help' => __( 'This will display the search box in the header', 'thshpr'  ),
 		'left-choice' => array(
@@ -25,9 +25,39 @@ $options = array(
 
 	'opt_header_show_social' =>array(
 		'type'  => 'switch',
-		'value' => 'Show',
+		'value' => '1',
 		'label' => __('Show Social media Icons in Header', 'thshpr'),
 		'help' => __( 'This will display the social media icons in the header', 'thshpr'  ),
+		'left-choice' => array(
+			'value' => '1',
+			'label' => __('Show', 'thshpr'),
+		),
+		'right-choice' => array(
+			'value' => '0',
+			'label' => __('Hide', 'thshpr'),
+		),
+	),
+
+	'opt_header_horizontal_center' =>array(
+		'type'  => 'switch',
+		'value' => '0',
+		'label' => __('Horizontally Center Header', 'thshpr'),
+		'help' => __( 'This will center the logo and top menu in two rows (logo first)', 'thshpr'  ),
+		'left-choice' => array(
+			'value' => '1',
+			'label' => __('Center', 'thshpr'),
+		),
+		'right-choice' => array(
+			'value' => '0',
+			'label' => __('Dont Center', 'thshpr'),
+		),
+	),
+
+	'opt_header_extra_top_bar_widgets' =>array(
+		'type'  => 'switch',
+		'value' => '0',
+		'label' => __('Show Extra Top Bar Widgets', 'thshpr'),
+		'help' => __( 'This will display the extra top bar above the header. It will consist of 2 50% width widget areas', 'thshpr'  ),
 		'left-choice' => array(
 			'value' => '1',
 			'label' => __('Show', 'thshpr'),
--- global-templates/header-nav.php
+++ global-templates/header-nav.php
@@ -13,6 +13,8 @@ if(function_exists( 'fw_get_db_customizer_option' ))
 	/*header options*/
 	$show_search=fw_get_db_customizer_option('opt_header_show_search');
 	$show_social=fw_get_db_customizer_option('opt_header_show_social');
+	$horizontal_center=fw_get_db_customizer_option('opt_header_horizontal_center');
+	$extra_top_bar_widget=fw_get_db_customizer_option('opt_header_extra_top_bar_widgets');
 	$social_twitter=fw_get_db_customizer_option('opt_social_twitter');
 	$social_facebook=fw_get_db_customizer_option('opt_social_facebook');
 	$social_googleplus=fw_get_db_customizer_option('opt_social_googleplus');
@@ -35,15 +37,6 @@ if(function_exists( 'fw_get_db_customizer_option' ))
 
 
 
-//$search=$nude_options['opt_header_show_search'];
-if($show_search==1)
-{
-	$left_column_width=3;
-}
-else
-{
-	$left_column_width=4;
-}
 ?>
     <div id="animatedModal">
 		<div class="close-icon background-dark close-animatedModal">
@@ -122,66 +115,74 @@ else
 	--><div class="fw-col-lg-6 pull-right">
 			<div class="pull-right right-extras">
 				<?php
-				echo'
-				<a id="nav-search" class="background-dark" href="#animatedModal"><i class="fa fa-search" aria-hidden="true"></i></a>
-				<div class="header-share-boxes featured-posts-grid-paragraph component-element">
-					<div class="absolute-container">
-						<i class="fa fa-plus icon start-icon background-accent"></i><!--';
-						if(isset($social_facebook))
-						{
-							echo'--><span class="social-box facebook"><a class="inner facebook-inner" href="'.urlencode($social_facebook).'"><i class="fa fa-facebook icon"></i></a></span><!--';
-						}
-						if(isset($social_twitter))
-						{
-							echo'--><span class="social-box facebook"><a class="inner twitter-inner" href="'.urlencode($social_twitter).'"><i class="fa fa-twitter icon"></i></a></span><!--';
-						}
-						if(isset($social_googleplus))
-						{
-							echo'--><span class="social-box googleplus"><a class="inner google-plus-inner" href="'.urlencode($social_googleplus).'"><i class="fa fa-google-plus icon"></i></a></span><!--';
-						}
-						if(isset($social_linkedin))
-						{
-							echo'--><span class="social-box linkedin"><a class="inner linked-in-inner" href="'.urlencode($social_linkedin).'"><i class="fa fa-linkedin icon"></i></a></span><!--';
-						}
-						if(isset($social_youtube))
-						{
-							echo'--><span class="social-box youtube"><a class="inner youtube-inner" href="'.urlencode($social_youtube).'"><i class="fa fa-youtube icon"></i></a></span><!--';
-						}
-						if(isset($social_instagram))
-						{
-							echo'--><span class="social-box instagram"><a class="inner instagram-inner" href="'.urlencode($social_instagram).'"><i class="fa fa-instagram icon"></i></a></span><!--';
-						}
-						if(isset($social_pinterest))
-						{
-							echo'--><span class="social-box pinterest"><a class="inner pinterest-inner" href="'.urlencode($social_pinterest).'"><i class="fa fa-pinterest icon"></i></a></span><!--';
-						}
-						if(isset($social_tumblr))
-						{
-							echo'--><span class="social-box tumblr"><a class="inner tumblr-inner" href="'.urlencode($social_tumblr).'"><i class="fa fa-tumblr icon"></i></a></span><!--';
-						}
-						if(isset($social_vine))
-						{
-							echo'--><span class="social-box vine"><a class="inner vine-inner" href="'.urlencode($social_vine).'"><i class="fa fa-vine icon"></i></a></span><!--';
-						}
-						if(isset($social_snapchat))
-						{
-							echo'--><span class="social-box snapchat"><a class="inner snapchat-inner" href="'.urlencode($social_snapchat).'"><i class="fa fa-snapchat icon"></i></a></span><!--';
-						}
-						if(isset($social_reddit))
-						{
-							echo'--><span class="social-box reddit"><a class="inner reddit-inner" href="'.urlencode($social_reddit).'"><i class="fa fa-reddit icon"></i></a></span><!--';
-						}
-						if(isset($social_flickr))
-						{
-							echo'--><span class="social-box flickr"><a class="inner flickr-inner" href="'.urlencode($social_flickr).'"><i class="fa fa-flickr icon"></i></a></span><!--';
-						}
-						if(isset($social_email))
-						{
-							echo'--><span class="social-box email"><a class="inner email-inner" href="'.urlencode($social_email).'"><i class="fa fa-at icon"></i></a></span>';
-						}
-						echo'
-					</div>
-				</div>';
+				if($show_search==1)
+				{
+					echo'
+					<a id="nav-search" class="background-dark" href="#animatedModal"><i class="fa fa-search" aria-hidden="true"></i></a>
+					';
+				}
+				if($show_social==1)
+				{
+					echo'
+					<div class="header-share-boxes featured-posts-grid-paragraph component-element">
+						<div class="absolute-container">
+							<i class="fa fa-plus icon start-icon background-accent"></i><!--';
+							if(isset($social_facebook))
+							{
+								echo'--><span class="social-box facebook"><a class="inner facebook-inner" href="'.urlencode($social_facebook).'"><i class="fa fa-facebook icon"></i></a></span><!--';
+							}
+							if(isset($social_twitter))
+							{
+								echo'--><span class="social-box facebook"><a class="inner twitter-inner" href="'.urlencode($social_twitter).'"><i class="fa fa-twitter icon"></i></a></span><!--';
+							}
+							if(isset($social_googleplus))
+							{
+								echo'--><span class="social-box googleplus"><a class="inner google-plus-inner" href="'.urlencode($social_googleplus).'"><i class="fa fa-google-plus icon"></i></a></span><!--';
+							}
+							if(isset($social_linkedin))
+							{
+								echo'--><span class="social-box linkedin"><a class="inner linked-in-inner" href="'.urlencode($social_linkedin).'"><i class="fa fa-linkedin icon"></i></a></span><!--';
+							}
+							if(isset($social_youtube))
+							{
+								echo'--><span class="social-box youtube"><a class="inner youtube-inner" href="'.urlencode($social_youtube).'"><i class="fa fa-youtube icon"></i></a></span><!--';
+							}
+							if(isset($social_instagram))
+							{
+								echo'--><span class="social-box instagram"><a class="inner instagram-inner" href="'.urlencode($social_instagram).'"><i class="fa fa-instagram icon"></i></a></span><!--';
+							}
+							if(isset($social_pinterest))
+							{
+								echo'--><span class="social-box pinterest"><a class="inner pinterest-inner" href="'.urlencode($social_pinterest).'"><i class="fa fa-pinterest icon"></i></a></span><!--';
+							}
+							if(isset($social_tumblr))
+							{
+								echo'--><span class="social-box tumblr"><a class="inner tumblr-inner" href="'.urlencode($social_tumblr).'"><i class="fa fa-tumblr icon"></i></a></span><!--';
+							}
+							if(isset($social_vine))
+							{
+								echo'--><span class="social-box vine"><a class="inner vine-inner" href="'.urlencode($social_vine).'"><i class="fa fa-vine icon"></i></a></span><!--';
+							}
+							if(isset($social_snapchat))
+							{
+								echo'--><span class="social-box snapchat"><a class="inner snapchat-inner" href="'.urlencode($social_snapchat).'"><i class="fa fa-snapchat icon"></i></a></span><!--';
+							}
+							if(isset($social_reddit))
+							{
+								echo'--><span class="social-box reddit"><a class="inner reddit-inner" href="'.urlencode($social_reddit).'"><i class="fa fa-reddit icon"></i></a></span><!--';
+							}
+							if(isset($social_flickr))
+							{
+								echo'--><span class="social-box flickr"><a class="inner flickr-inner" href="'.urlencode($social_flickr).'"><i class="fa fa-flickr icon"></i></a></span><!--';
+							}
+							if(isset($social_email))
+							{
+								echo'--><span class="social-box email"><a class="inner email-inner" href="'.urlencode($social_email).'"><i class="fa fa-at icon"></i></a></span>';
+							}
+							echo'
+						</div>
+					</div>';
+				}
 				?>
 
 
