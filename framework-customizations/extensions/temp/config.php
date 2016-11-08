ed57a629e3fdc349c4fbc7977620ad0356b7de20
Unknown <roates@gmail.com>
2016-11-08 18:44:23 +0100

    incomplete, adding new grid items to builder

---
+++ framework-customizations/extensions/temp/config.php
@@ -0,0 +1,65 @@
+<?php if (!defined('FW')) die('Forbidden');
+
+$cfg = array();
+
+$cfg['default_item_widths'] = array(
+    /**
+     * Copy/Paste here default columns https://github.com/ThemeFuse/Unyson-Builder-Extension/blob/master/config.php
+     * and add, remove or change them
+     */
+	 '1_6' => array(
+		'title'          => '1/6',
+		'backend_class'  => 'fw-col-sm-2',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-2',
+	),
+	'1_5' => array(
+		'title'          => '1/5',
+		'backend_class'  => 'fw-col-sm-15',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-15',
+	),
+	'2_5' => array(
+		'title'          => '2/5',
+		'backend_class'  => 'fw-col-sm-25',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-25',
+	),
+	'3_5' => array(
+		'title'          => '3/5',
+		'backend_class'  => 'fw-col-sm-35',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-35',
+	),
+	'4_5' => array(
+		'title'          => '4/5',
+		'backend_class'  => 'fw-col-sm-45',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-45',
+	),
+	'1_4' => array(
+		'title'          => '1/4',
+		'backend_class'  => 'fw-col-sm-3',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-3',
+	),
+	'1_3' => array(
+		'title'          => '1/3',
+		'backend_class'  => 'fw-col-sm-4',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-4',
+	),
+	'1_2' => array(
+		'title'          => '1/2',
+		'backend_class'  => 'fw-col-sm-6',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-6',
+	),
+	'2_3' => array(
+		'title'          => '2/3',
+		'backend_class'  => 'fw-col-sm-8',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-8',
+	),
+	'3_4' => array(
+		'title'          => '3/4',
+		'backend_class'  => 'fw-col-sm-9',
+		'frontend_class' => 'fw-col-xs-12 fw-col-sm-9',
+	),
+	'1_1' => array(
+		'title'          => '1/1',
+		'backend_class'  => 'fw-col-sm-12',
+		'frontend_class' => 'fw-col-xs-12',
+	),
+);
